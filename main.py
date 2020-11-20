0808f34c1586022f5fb869c3b7afb2155785ae9d
Mikhail <semdestroyer@yandex.ru>
2020-11-20 20:10:23 +0300

    first buttons added

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
 import telebot
-bot = telebot.TeleBot('')
+bot = telebot.TeleBot('1408437105:AAERPrZPLbkGoHN9HzObvYScyGBQNbwZzoY')
+
+comps=["программирование", "unix", "программная инжнерия", ]
+
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.send_message(message.chat.id, "Привет")
+    msg="Приветствую тебя студент, здесь ты можешь задавать вопросы другим студентам или отвечать сам на то, в чём разбираешься."
+    msg+="Нажми \"добавить компетенцию\", чтобы указать, чем владеешь или сразу задай вопрос"
+    markup=telebot.types.ReplyKeyboardMarkup()
+    addSkill=telebot.types.KeyboardButton("Добавить компетенцию")
+    ask=telebot.types.KeyboardButton("Задать вопрос")
+    markup.add(addSkill,ask)
+    bot.send_message(message.chat.id, msg, reply_markup=markup)
+
+
+
 bot.polling()
